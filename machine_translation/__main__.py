761fd307113a9b518a43b3f3a5c243ec91082b80
Eder Santana <edersantanajunior@hotmail.com>
2015-11-11 17:32:06 -0500

    Update __main__.py

--- machine_translation/__main__.py
+++ machine_translation/__main__.py
@@ -1,10 +1,10 @@
 """Encoder-Decoder with search for machine translation.
 
-In this demo, encoder-decoder architecture with attention mechanism is used for
+In this demo an encoder-decoder architecture with an attention mechanism is used for
 machine translation. The attention mechanism is implemented according to
 [BCB]_. The training data used is WMT15 Czech to English corpus, which you have
 to download, preprocess and put to your 'datadir' in the config file. Note
-that, you can use `prepare_data.py` script to download and apply all the
+that you can use `prepare_data.py` script to download and apply all the
 preprocessing steps needed automatically.  Please see `prepare_data.py` for
 further options of preprocessing.
 
